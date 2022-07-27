26dcd3abd9ceea739f22a3f29655a113f77567b1
Sankha Rathnayake <2017cs143@stu.ucsc.cmb.ac.lk>
2022-07-27 08:22:10 +0530

    404 page added

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import { Container, Box } from "@mui/material";
+import React from "react";
+
+// styling
+import "./styles/NotFound.css";
+
+// assets
+import Picture from "../assets/404.png";
 
 export default function NotFound() {
   return (
-    <div>NotFound</div>
-  )
+    <Box>
+      <Container>
+        <img className="image404" src={Picture} />
+      </Container>
+    </Box>
+  );
 }
